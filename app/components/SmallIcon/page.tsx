cd439c654ebe5f977e06d94820f85cc3e7160d72
minhanPark <alsgks1218@naver.com>
2023-08-08 13:35:08 +0900

    작은 터미널 아이콘 형태 갖추도록 css 추가하고 inline-block일때도 drag 테스트

--- app/components/SmallIcon/page.tsx
+++ app/components/SmallIcon/page.tsx
@@ -1,14 +1,24 @@
 "use client";
 
 import { BsFillTerminalFill } from "react-icons/bs";
+import { FaTerminal } from "react-icons/fa";
 import { IconContext } from "react-icons";
+import { motion } from "framer-motion";
 
 export default function SmallIcon() {
   return (
-    <IconContext.Provider value={{ color: "red" }}>
-      <div>
-        <BsFillTerminalFill size={50} />
-      </div>
-    </IconContext.Provider>
+    <motion.div
+      style={{
+        background: "black",
+        display: "inline-block",
+        padding: 5,
+        borderRadius: 5,
+        border: "3px solid gray",
+      }}
+      whileHover={{ scale: 1.02 }}
+      drag
+    >
+      <FaTerminal size={50} color="white" />
+    </motion.div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import SmallIcon from "./components/SmallIcon/page";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen bg-slate-700">
       <SmallIcon />
     </div>
   );
