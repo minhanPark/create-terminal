291d92a64abf47ab061439546934f539b1eae85f
minhanPark <alsgks1218@naver.com>
2023-08-10 17:57:34 +0900

    드래그 확인, x 아이콘 테스트

--- app/components/SmallTerminal/page.tsx
+++ app/components/SmallTerminal/page.tsx
@@ -1,11 +1,28 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { IoCloseSharp } from "react-icons/io5";
+import { IconContext } from "react-icons";
 
 export default function SmallTerminal() {
   return (
-    <motion.div style={{ width: "500px", height: "500px" }}>
-      <div style={{ backgroundColor: "#dfe6e9" }}>헤더</div>
+    <motion.div
+      style={{ width: "500px", height: "500px" }}
+      className="flex flex-col"
+      drag
+      dragMomentum={false}
+    >
+      <div style={{ backgroundColor: "#dfe6e9", height: "25px" }}>
+        <div>
+          <div className="bg-red-600 inline-block rounded-full">
+            <IoCloseSharp />
+          </div>
+        </div>
+      </div>
+      <div
+        className="bg-red-300 grow"
+        onPointerDownCapture={(e) => e.stopPropagation()}
+      ></div>
     </motion.div>
   );
 }
