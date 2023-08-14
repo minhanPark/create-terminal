1945d14fd8ad0a4550aa50b6b42d0c0a286345cb
minhanPark <alsgks1218@naver.com>
2023-08-14 10:43:45 +0900

    이벤트를 통해서 mouse hover 시 아이콘 변경하도록 확인

--- app/components/SmallTerminal/page.tsx
+++ app/components/SmallTerminal/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { IoCloseSharp } from "react-icons/io5";
 import { BiMinus } from "react-icons/bi";
@@ -8,6 +9,7 @@ import { IconContext } from "react-icons";
 import { VscBlank } from "react-icons/vsc";
 
 export default function SmallTerminal() {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <motion.div
       style={{ width: "500px", height: "500px" }}
@@ -16,16 +18,29 @@ export default function SmallTerminal() {
       dragMomentum={false}
     >
       <div style={{ backgroundColor: "#dfe6e9", height: "25px" }}>
-        <div>
-          <div className="bg-red-600 inline-block rounded-full">
+        <div
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          className="border-slate-400 border border-solid flex w-16 h-full items-center justify-evenly"
+        >
+          <div
+            className="bg-red-600 inline-block rounded-full"
+            onPointerDownCapture={(e) => e.stopPropagation()}
+          >
+            {isHovered ? <IoCloseSharp /> : <VscBlank />}
             {/* 크기를 유지해주는 blank icon이 있어서 hover 시 바꿔주면 될듯하다 */}
-            <VscBlank />
           </div>
-          <div className="bg-yellow-600 inline-block rounded-full">
-            <BiMinus />
+          <div
+            className="bg-yellow-600 inline-block rounded-full"
+            onPointerDownCapture={(e) => e.stopPropagation()}
+          >
+            {isHovered ? <BiMinus /> : <VscBlank />}
           </div>
-          <div className="bg-blue-600 inline-block rounded-full">
-            <AiOutlineExpandAlt />
+          <div
+            className="bg-blue-600 inline-block rounded-full"
+            onPointerDownCapture={(e) => e.stopPropagation()}
+          >
+            {isHovered ? <AiOutlineExpandAlt /> : <VscBlank />}
           </div>
         </div>
       </div>
